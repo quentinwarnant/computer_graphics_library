a14fdd799db6c655c1a76b4cf64afeea5de51482
Quentin Warnant - Mac <lequentin@gmail.com>
2020-08-01 21:11:10 +0100

    unity upgrade to 2020.1.0f1 started work on dual refraction glass effect

---
+++ Unity_vfx/Assets/Effects/Refraction/DepthRender.cs
@@ -0,0 +1,143 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
+
+
+public class DepthRender : UnityEngine.Rendering.Universal.ScriptableRendererFeature
+{
+    [System.Serializable]
+    public class DepthRenderSettings
+    {
+        public RenderPassEvent When;
+        public bool DepthInversed;
+        public Material DepthMaterial;
+    }
+
+    class DepthRenderPass : ScriptableRenderPass
+    {
+        string m_tag;
+        DepthRenderSettings m_settings; 
+
+        RenderTargetHandle m_QDepthTexture;
+        RenderTextureDescriptor m_cameraTargetDesc;
+
+        DrawingSettings m_drawingSettings;
+        FilteringSettings m_filteringSettings;
+
+        ShaderTagId m_ShaderTagId = new ShaderTagId("DepthOnly");
+
+        RenderTargetIdentifier m_renderTargetIdentifier;
+
+        public DepthRenderPass(string tag, DepthRenderSettings settings)
+        {
+            m_tag = tag;
+            m_settings = settings;
+            // Configures where the render pass should be injected.
+            renderPassEvent = settings.When;
+
+            m_filteringSettings = new FilteringSettings(RenderQueueRange.opaque, -1);
+        }
+
+        public void Setup(RenderTextureDescriptor cameraTargetDesc, RenderTargetIdentifier renderTargetIdentifier, RenderTargetHandle RThandle)
+        {
+            m_cameraTargetDesc = cameraTargetDesc;
+            m_cameraTargetDesc.colorFormat = RenderTextureFormat.ARGB32;
+            m_cameraTargetDesc.depthBufferBits = 32;
+
+            m_renderTargetIdentifier = renderTargetIdentifier;
+            m_QDepthTexture = RThandle;
+
+        }
+
+        // This method is called before executing the render pass. 
+        // It can be used to configure render targets and their clear state. Also to create temporary render target textures.
+        // When empty this render pass will render to the active camera render target.
+        // You should never call CommandBuffer.SetRenderTarget. Instead call <c>ConfigureTarget</c> and <c>ConfigureClear</c>.
+        // The render pipeline will ensure target setup and clearing happens in an performance manner.
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            var desc = m_cameraTargetDesc;
+            cmd.GetTemporaryRT(m_QDepthTexture.id, desc.width, desc.height, desc.depthBufferBits, FilterMode.Point);
+            ConfigureTarget(m_QDepthTexture.Identifier());
+            ConfigureClear(ClearFlag.All, Color.black);
+        }
+
+        // Here you can implement the rendering logic.
+        // Use <c>ScriptableRenderContext</c> to issue drawing commands or execute command buffers
+        // https://docs.unity3d.com/ScriptReference/Rendering.ScriptableRenderContext.html
+        // You don't have to call ScriptableRenderContext.submit, the render pipeline will call it at specific points in the pipeline.
+        public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
+        {
+            CommandBuffer cmdBuff = CommandBufferPool.Get(m_tag);
+
+            //cmdBuff.name = "testQ";
+            //cmdBuff.Blit(m_renderTargetIdentifier, m_QDepthTexture.Identifier(), m_settings.DepthMaterial);
+            //context.ExecuteCommandBuffer(cmdBuff);
+            //cmdBuff.Clear();
+
+            using (new ProfilingSample(cmdBuff, m_tag))
+            {
+                context.ExecuteCommandBuffer(cmdBuff);
+                cmdBuff.Clear();
+                cmdBuff.name = m_tag;// "Blit Depth backwards";
+
+                var sortFlags = renderingData.cameraData.defaultOpaqueSortFlags;
+                var drawSettings = CreateDrawingSettings(m_ShaderTagId, ref renderingData, sortFlags);
+                drawSettings.perObjectData = PerObjectData.None;
+
+
+                ref CameraData cameraData = ref renderingData.cameraData;
+                Camera camera = cameraData.camera;
+                if (cameraData.isStereoEnabled)
+                    context.StartMultiEye(camera);
+
+
+                drawSettings.overrideMaterial = m_settings.DepthMaterial;
+
+
+                context.DrawRenderers(renderingData.cullResults, ref drawSettings,
+                    ref m_filteringSettings);
+
+                cmdBuff.SetGlobalTexture("_CameraDepthNormalsTexture2", m_QDepthTexture.id);
+
+
+            }
+            context.ExecuteCommandBuffer(cmdBuff);
+
+            //cmdBuff.Clear();
+            CommandBufferPool.Release(cmdBuff);
+        }
+
+        /// Cleanup any allocated resources that were created during the execution of this render pass.
+        public override void FrameCleanup(CommandBuffer cmd)
+        {
+            if (m_QDepthTexture != RenderTargetHandle.CameraTarget)
+            {
+                cmd.ReleaseTemporaryRT(m_QDepthTexture.id);
+                m_QDepthTexture = RenderTargetHandle.CameraTarget;
+            }
+        }
+    }
+
+    public DepthRenderSettings settings = new DepthRenderSettings();
+    DepthRenderPass m_scriptablePass;
+    RenderTargetHandle m_rthandle;
+
+    public override void Create()
+    {
+        m_rthandle.Init("QDepthTex");
+        m_scriptablePass = new DepthRenderPass(name, settings);
+    }
+    
+    // Here you can inject one or multiple render passes in the renderer.
+    // This method is called when setting up the renderer once per-camera.
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        var cameraTargetIdentifier = renderer.cameraColorTarget;
+        var cameraTargetDesc = renderingData.cameraData.cameraTargetDescriptor;
+        m_scriptablePass.Setup(cameraTargetDesc, cameraTargetIdentifier, m_rthandle);
+        renderer.EnqueuePass(m_scriptablePass);
+    }
+}
+
+
---
+++ Unity_vfx/Assets/Effects/Refraction/Refraction.shader
@@ -0,0 +1,72 @@
+﻿Shader "Q/Refraction"
+{
+    Properties
+    {
+    }
+    SubShader
+    {
+        Tags { "RenderType"="Opaque" }
+        LOD 100
+
+        ZWrite On
+        ZTest Less
+        Cull Back
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+                float3 normal: NORMAL;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float3 lighting : COLOR;
+            };
+
+            sampler2D _MainTex;
+            float4 _MainTex_ST;
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                float4 inversedPos = v.vertex;
+
+//TODO: FIGURE OUT HOW TO INVERSE THE RENDERING ON THE Z AXIS IN VIEW SPACE
+// THEN DEPTH BUFFER WILL BE CORRECT FOR FIRST PASS OF EFFECT!
+                float4x4 invMatrix = float4x4(  -1,0,0,0,
+                                                0,1,0,0,
+                                                0,0,1,0,
+                                                0,0,0,1);
+
+                float4x4 inversedZMVP = mul(UNITY_MATRIX_P ,  mul( UNITY_MATRIX_V ,UNITY_MATRIX_M) );
+                inversedZMVP = mul( inversedZMVP, invMatrix);
+                o.vertex =  mul( inversedZMVP, v.vertex);
+  
+//                o.vertex =  UnityObjectToClipPos(inversedPos);
+
+                float3 worldNormal = UnityObjectToWorldNormal(v.normal);
+				float3 NdotL = max(0,dot( worldNormal, _WorldSpaceLightPos0 ));
+				
+                o.lighting = (NdotL  * fixed3(1,1,1)); 
+
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                fixed3 col = 0.5;
+                return fixed4(col,1);
+            }
+            ENDCG
+        }
+    }
+}
