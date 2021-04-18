cb1c10f3f80fd30879af10dbfd9c97aaf55232e4
Quentin Warnant - Mac <lequentin@gmail.com>
2021-04-18 13:15:57 +0100

    Start setup for fluid sim effect with compute shader, manager & ray march fragment shader

---
+++ Unity_vfx/Assets/Effects/FluidSim/FluidSimManager.cs
@@ -0,0 +1,117 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class FluidSimManager : MonoBehaviour
+{
+    [SerializeField] ComputeShader m_fluidSimCS;
+
+    [SerializeField] int m_volumeSize = 5;
+    [SerializeField] float m_diffusion = 1.0f;
+    [SerializeField] float m_viscosity = 0.1f;
+
+    // 16 x 16 x 16 X 16 (threads) = 65536 cells in sim
+    int m_dimensionGroupCount = 16;
+    int m_cellsPerGroup = 8; // threads per group
+        
+
+    ComputeBuffer m_fluidPrevDensityBuffer;
+    ComputeBuffer m_fluidDensityBuffer;
+    ComputeBuffer m_fluidPrevVelocityBuffer;
+    ComputeBuffer m_fluidVelocityBuffer;
+
+    RenderTexture m_fluidRT;
+    [SerializeField] MeshRenderer m_renderer;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        int N = m_dimensionGroupCount;
+        int cellCount = N * N * N * m_cellsPerGroup;
+
+        int sizeDensityData = sizeof(float);
+        m_fluidPrevDensityBuffer = new ComputeBuffer(cellCount, sizeDensityData);
+        m_fluidDensityBuffer = new ComputeBuffer(cellCount, sizeDensityData);
+        InitDensityBuffersdata(m_fluidPrevDensityBuffer, cellCount );
+        InitDensityBuffersdata(m_fluidDensityBuffer, cellCount );
+
+        int sizeVelocityyData = sizeof(float) * 3;
+        m_fluidPrevVelocityBuffer = new ComputeBuffer(cellCount, sizeVelocityyData);
+        m_fluidVelocityBuffer = new ComputeBuffer(cellCount, sizeVelocityyData);
+        InitVelocityBuffersdata(m_fluidPrevVelocityBuffer, cellCount );
+        InitVelocityBuffersdata(m_fluidVelocityBuffer, cellCount );
+
+
+        m_fluidSimCS.SetBuffer(0, "PrevDens", m_fluidPrevDensityBuffer);
+        m_fluidSimCS.SetBuffer(0, "Dens", m_fluidDensityBuffer);
+        m_fluidSimCS.SetBuffer(0, "PrevVel", m_fluidPrevVelocityBuffer);
+        m_fluidSimCS.SetBuffer(0, "Vel", m_fluidVelocityBuffer);
+
+        m_fluidSimCS.SetInt("Size", m_volumeSize);
+        m_fluidSimCS.SetFloat("Diffusion", m_diffusion);
+        m_fluidSimCS.SetFloat("Viscosity", m_viscosity);
+        m_fluidSimCS.SetFloat("dt", 0);
+
+        // Output
+        int rtDimension = N * m_cellsPerGroup;
+        RenderTextureDescriptor RTDesc = new RenderTextureDescriptor(rtDimension, rtDimension, RenderTextureFormat.RHalf, 0);
+        RTDesc.dimension = TextureDimension.Tex3D;
+        RTDesc.enableRandomWrite = true;
+        RTDesc.volumeDepth = rtDimension;
+
+        m_fluidRT = new RenderTexture(RTDesc);
+        m_fluidRT.filterMode = FilterMode.Point;
+        m_fluidRT.Create();
+
+        m_fluidSimCS.SetTexture(0, "Result", m_fluidRT);
+
+        m_renderer.material.SetTexture("_FluidSimTex", m_fluidRT);
+
+    }
+
+    void InitDensityBuffersdata(ComputeBuffer densityBuffer, int count)
+    {
+        float[] initialData = new float[count];
+        densityBuffer.SetData(initialData);
+    }
+
+    void InitVelocityBuffersdata(ComputeBuffer velocityBuffer, int count)
+    {
+        float[] initialData = new float[count * 3];
+        velocityBuffer.SetData(initialData);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_fluidSimCS.SetFloat("dt", Time.deltaTime);
+        m_fluidSimCS.SetFloat("time", Time.time);
+
+
+        RunComputeShader();
+    }
+
+    void RunComputeShader()
+    {
+        int ThreadGroupX = m_dimensionGroupCount;
+        int ThreadGroupY = m_dimensionGroupCount;
+        int ThreadGroupZ = m_dimensionGroupCount;
+        
+        m_fluidSimCS.Dispatch(0, ThreadGroupX, ThreadGroupY, ThreadGroupZ);
+    }
+
+    void OnDestroy()
+    {
+        m_fluidPrevDensityBuffer.Release();
+        m_fluidDensityBuffer.Release();
+        m_fluidPrevVelocityBuffer.Release();
+        m_fluidVelocityBuffer.Release();
+
+        m_fluidRT.Release();
+        m_fluidRT = null;
+    }
+
+}
---
+++ Unity_vfx/Assets/Shaders/FluidSim/FluidSim.shader
@@ -0,0 +1,113 @@
+﻿Shader "Q/FluidSim"
+{
+    Properties
+    {
+//        _MainTex ("Texture", 2D) = "white" {}
+        _FluidSimTex("Fluid Sim 3D", 3D) = "white" {}
+        _StepSize ("Step Size", float) = 0.01
+        _Alpha("Alpha", Range(0.001,0.4)) = 0.02
+    }
+    SubShader
+    {
+        Tags {
+             "RenderType"="Transparent" 
+
+        }
+        Blend SrcAlpha OneMinusSrcAlpha
+        LOD 100
+
+        Pass
+        {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            #include "UnityCG.cginc"
+
+            // Maximum amount of raymarching samples
+            #define MAX_STEP_COUNT 128
+            // Allowed floating point inaccuracy
+            #define EPSILON 0.00001f
+
+
+            sampler3D _FluidSimTex;
+            float4 _FluidSimTex_ST;
+
+            float _StepSize;
+            float _Alpha;
+
+            struct appdata
+            {
+                float4 vertex : POSITION;
+            };
+
+            struct v2f
+            {
+                float4 vertex : SV_POSITION;
+                float4 vertex_ObjSpace : TEXCOORD0;
+                float3 vectorToSurface : TEXCOORD1;
+            };
+
+            float4 BlendUnder(float4 color, float4 newColor)
+            {
+                color.rgb += (1.0 - color.a) * newColor.a * newColor.rgb;
+                color.a += (1.0 - color.a) * newColor.a;
+                return color;
+            }
+
+            v2f vert (appdata v)
+            {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+//                o.vertex_ObjSpace = ((v.vertex * 0.5) + 0.5);
+                o.vertex_ObjSpace = v.vertex;
+
+                float3 worldVertex = mul(unity_ObjectToWorld, v.vertex).xyz;
+                o.vectorToSurface = worldVertex - _WorldSpaceCameraPos;
+
+
+                return o;
+            }
+
+            fixed4 frag (v2f i) : SV_Target
+            {
+                
+                //fixed4 col = float4(0,0,0,1);
+                //col.rgb = tex3D(_FluidSimTex, i.vertex_ObjSpace.xyz+float3(0.5f, 0.5f, 0.5f));
+                //col.rgb = i.vertex_ObjSpace.xyz + .5 ;
+                //return col;
+                
+                
+
+                 // Start raymarching at the front surface of the object
+                float3 rayOrigin = i.vertex_ObjSpace;
+
+                // Use vector from camera to object surface to get ray direction
+                float3 rayDirection = mul(unity_WorldToObject, float4(normalize(i.vectorToSurface), 1));
+
+                float4 color = float4(0, 0, 0, 0);
+                float3 samplePosition = rayOrigin;
+
+                // Raymarch through object space
+                for (int i = 0; i < MAX_STEP_COUNT; i++)
+                {
+                    // Accumulate color only within unit cube bounds
+                    if(max(abs(samplePosition.x), max(abs(samplePosition.y), abs(samplePosition.z))) < 0.5f + EPSILON)
+                    {
+                        float4 sampledColor = abs(tex3D(_FluidSimTex, samplePosition + float3(0.5f, 0.5f, 0.5f)).rrrr);
+                        sampledColor.a *= _Alpha;
+                        color = BlendUnder(color, sampledColor);
+                        samplePosition += rayDirection * _StepSize;
+                    }
+                }
+
+                color.a = color.r;
+
+                return color;
+
+
+            }
+            ENDCG
+        }
+    }
+}
